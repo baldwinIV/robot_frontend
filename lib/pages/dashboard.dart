650ef445d4c2515d95a3351a6ecdb1d490869832
baldwinIV <ksanghun10@gmail.com>
2021-07-06 02:38:42 +0900

    DashBoard 추가, 기본 main 화면 제작

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:robot_frontend/main.dart';
+import 'package:provider/provider.dart' ;
+
+
+class DashBoard extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HelloWorld"),
+      ),
+    );
+  }
+
+}
