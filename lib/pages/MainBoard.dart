8e5b4ad5d58b07d4a4a3a7c6ea2cffce34fe63ed
baldwinIV <ksanghun10@gmail.com>
2021-07-07 00:08:50 +0900

    MainBoard.dart에서 GetWidget 함수 생성,

--- lib/pages/MainBoard.dart
+++ lib/pages/MainBoard.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:robot_frontend/menu_widget/SensorMqtt.dart';
-import 'package:robot_frontend/menu_widget/Menu.dart';
+import 'package:robot_frontend/Menu.dart';
 import 'package:provider/provider.dart';
 
 class MainBoard extends StatelessWidget {
-
+  Widget getWidget(String widgetName) {
+    int x = 2;
+    switch(widgetName){
+      case 'Sensor':
+        return SensorMqtt(title: "Hello");
+      default :
+        return Text("HelloWorld");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +27,7 @@ class MainBoard extends StatelessWidget {
               Expanded(
                 //여기에 뭐가 렌더링 되면 될듯!
                 flex: 5,
-                child: SensorMqtt(title: "Hello"),
+                child: getWidget('Sensor'),
               )
             ],
           )),
