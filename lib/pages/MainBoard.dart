8e1b3113e1ecde02481045125b2d4007ac6942d0
baldwinIV <ksanghun10@gmail.com>
2021-07-15 03:55:04 +0900

    add Header on MainBOard

--- lib/pages/MainBoard.dart
+++ lib/pages/MainBoard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:robot_frontend/menu_widget/SensorMqtt.dart';
 import 'package:robot_frontend/menu_widget/Dashboard.dart';
 import 'package:robot_frontend/Menu.dart';
+import 'package:robot_frontend/pages/MainBoardHeader.dart';
 import 'package:provider/provider.dart';
 import 'package:robot_frontend/providers/SwitchProvider.dart';
 
@@ -13,21 +14,35 @@ class MainBoard extends StatelessWidget {
       case "Sensor":
         return SensorMqtt(title: "Hello");
       case "ParsedMap":
-        return Text("ParsedMap",style: TextStyle(fontSize: 30),);
+        return Text(
+          "ParsedMap",
+          style: TextStyle(fontSize: 30),
+        );
       case "History":
-        return Text("History",style: TextStyle(fontSize: 30),);
+        return Text(
+          "History",
+          style: TextStyle(fontSize: 30),
+        );
       case "Settings":
-        return Text("Settings",style: TextStyle(fontSize: 30),);
+        return Text(
+          "Settings",
+          style: TextStyle(fontSize: 30),
+        );
       default:
-        return Text("default",style: TextStyle(fontSize: 30),);
+        return Text(
+          "default",
+          style: TextStyle(fontSize: 30),
+        );
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Expanded(
             flex: 1,
@@ -36,8 +51,20 @@ class MainBoard extends StatelessWidget {
           Expanded(
             flex: 5,
             child: Consumer<SwitchProvider>(
-              builder: (context, spProvider, child) =>
-                  getWidget(spProvider.getRouteName()),
+              builder: (context, spProvider, child) => Column(
+                children: [
+                  MainBoardHeader(),
+                  Expanded(
+                    flex: 2,
+                    child: Text("a"),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: getWidget(spProvider.getRouteName()),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
             ),
           )
         ],
---
+++ lib/pages/MainBoardHeader.dart
@@ -0,0 +1,35 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:robot_frontend/menu_widget/SensorMqtt.dart';
+import 'package:mqtt_client/mqtt_browser_client.dart';
+import 'package:robot_frontend/providers/MqttProvider.dart';
+
+class MainBoardHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<MqttProvider>(
+          builder: (context, mqttProvider, child) => Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF00FF00),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1.0,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "DASHBOARD",
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    Text(mqttProvider.getTopic() + "   "),
+                    Text(mqttProvider.getMqttHostName() + "   "),
+                    Text(mqttProvider.getMqttPort()),
+                  ],
+                ),
+              )),
+    );
+  }
+}
