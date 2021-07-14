994117e1bdd89504e51536b6919f38e44590e9b3
baldwinIV <ksanghun10@gmail.com>
2021-07-15 03:56:07 +0900

    add ParsedMap.dart

---
+++ lib/menu_widget/ParsedMap.dart
@@ -0,0 +1,16 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:robot_frontend/menu_widget/SensorMqtt.dart';
+import 'package:mqtt_client/mqtt_browser_client.dart';
+
+class ParsedMap extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+
+}
